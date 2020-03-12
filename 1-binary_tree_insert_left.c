b94330d297060ed7885944309e6a0b008a29cd75
khaldi505 <habetlbrika69@gmail.com>
2020-03-12 15:57:37 +0100

    coding style fixed

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,8 +8,6 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-if (parent == NULL)
-return (NULL);
 binary_tree_t *left_node;
 left_node = malloc(sizeof(binary_tree_t));
 if (left_node == NULL)
@@ -18,12 +16,17 @@ free(left_node);
 return (NULL);
 }
 
+if (parent == NULL)
+return (NULL);
+
 left_node->parent = parent;
 left_node->n = value;
 left_node->right = NULL;
 left_node->left = parent->left;
 parent->left = left_node;
+
 if (left_node->left)
 left_node->left->parent = left_node;
+
 return (left_node);
 }
