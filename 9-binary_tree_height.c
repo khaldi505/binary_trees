f8e6f1e53192153305329c7cf1106d6ce666321e
yassineaboub <53088418+yassineaboub@users.noreply.github.com>
2020-03-13 15:38:48 +0100

    3ab3oub flexing on the beat ayooo!!

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+/**
+ *binary_tree_height - height of a binary
+ *@tree: pointer
+ *Return: size_t karzetny wallah
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+size_t left;
+size_t right;
+
+if (!tree || (!(tree->left) && !(tree->right)))
+return (0);
+
+else
+{
+left = binary_tree_height(tree->left);
+right = binary_tree_height(tree->right);
+if (left > right)
+{
+return (left + 1);
+}
+else
+{
+return (right + 1);
+}
+}
+}
