da23ed55681009547b58179090cf76690967d08c
khaldi505 <habetlbrika69@gmail.com>
2020-03-13 20:30:58 +0100

    10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -6,16 +6,9 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-size_t left, right;
 if (!tree)
 return (0);
 if (tree->parent == NULL)
 return (0);
-
-left = binary_tree_depth(tree->left);
-right = binary_tree_depth(tree->right);
-if (left > right)
-return (left + 1);
-return (right + 1);
-
+return (binary_tree_depth(tree->parent) + 1);
 }
