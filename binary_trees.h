51b25d72ca34560f203d133434b969dea9352550
yassineaboub <53088418+yassineaboub@users.noreply.github.com>
2020-03-13 03:50:34 +0100

    added new function

--- binary_trees.h
+++ binary_trees.h
@@ -20,6 +20,7 @@ typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 void binary_tree_print(const binary_tree_t *tree);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+int binary_tree_is_leaf(const binary_tree_t *node);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
