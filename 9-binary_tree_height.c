76d215ef8121159ffb77f3b1ffadcae3d9261159
yassineaboub <53088418+yassineaboub@users.noreply.github.com>
2020-03-13 16:22:49 +0100

    wgt

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 /**
  *binary_tree_height - height of a binary
- *@tree: pointer
- *Return: size_t karzetny wallah
+ *@tree: pointer to the root node of the measure the height.
+ *Return: if tree is NULL, your function must return 0
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
