2d02acc35c7ff1257443b291705db91af3b84299
khaldi505 <habetlbrika69@gmail.com>
2020-04-09 02:39:03 +0200

    16-binary_tree_is_perfect.c

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,46 @@
+#include "binary_trees.h"
+/**
+ *height - returns the height
+ *of a tree or a sub tree
+ *@tree: pointer to the tree
+ *Return: the height
+ */
+int height(const binary_tree_t *tree)
+{
+int left, right;
+
+if (!tree)
+return (0);
+
+left = height(tree->left);
+right = height(tree->right);
+if (left > right)
+return (left + 1);
+return (right + 1);
+}
+/**
+ *binary_tree_balance - a function that
+ * measures the balance factor of a binary tree
+ *@tree: pointer to the root of the tree
+ *Return: the height of the left nodes - the height of the right nodes
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+if (!tree)
+return (0);
+
+return (height(tree->left) - height(tree->right));
+}
+/**
+ *binary_tree_is_perfect - checks if the binary tree is perfect
+ *@tree: pointer to the tree
+ *Return: 0 or 1
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+if (binary_tree_balance(tree) == 0)
+return (1);
+
+return (0);
+}
