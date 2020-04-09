ec3d3217584c2cbefec822927d54b6f7253bd4ed
yassineaboub <yassineaboub@live.fr>
2020-04-09 23:08:14 +0000

    17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -9,7 +9,7 @@
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 if (node == NULL || node->parent == NULL)
-return NULL;
+return (NULL);
 if (node->parent->right == node)
 {
 return (node->parent->left);
@@ -19,6 +19,6 @@ return (node->parent->right);
 {
 if (node->parent == NULL
 || node->parent->parent == NULL)
-return NULL;
+return (NULL);
 }
 }
